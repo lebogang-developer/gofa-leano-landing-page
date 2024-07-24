2bb480fac6cdeadd05fcc63ba08bc68716052c77
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-07-24 05:16:08 +0200

    Js added and new changes

--- home.js
+++ home.js
@@ -1,1 +1,10 @@
-// 
+// Mobile Menu
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hambugerButton = document.querySelector('.hambuger-btn');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  document.addEventListener('click', () =>
+    mobileMenu.classList.toggle('active')
+  );
+});
